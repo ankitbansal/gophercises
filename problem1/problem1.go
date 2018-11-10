71d140b64b7e58feefb081f1b3e310ea217a6702
Ankit Bansal <ankit.a.bansal@oracle.com>
2018-11-10 23:11:47 +0530

    Added timeout functionality

--- problem1/problem1.go
+++ problem1/problem1.go
@@ -14,11 +14,13 @@ import (
 var (
 	filePath 	string
 	random		bool
+	timeOut		int
 )
 
 func init() {
 	flag.StringVar(&filePath, "filePath", "csv/simple.csv", "Name of csv File from which to read questions")
 	flag.BoolVar(&random, "randomize", true, "boolean flag to specify whether to randomize questions. Defaults to true")
+	flag.IntVar(&timeOut, "timeout", 50, "time out for quiz in seconds. Default is 50")
 	flag.Parse()
 }
 
@@ -34,6 +36,14 @@ func main() {
 		})
 	}
 
+	timer := time.NewTimer(time.Second * time.Duration(timeOut))
+	defer timer.Stop()
+
+	go func() {
+		<-timer.C
+		fmt.Printf("You got %d answers right out of %d", correctAnswers, len(records))
+		os.Exit(0)
+	}()
 
 	for i:= 0; i < len(records); i++ {
 		record := records[i]
